7d36f6a5f242f8c27b955846232f4b8ffa85ef05
tigerhostsolutions <jgrasty@tigerhost.domains>
2025-01-07 02:10:10 -0500

    updated: index.html updated: style.css updated: index.js-images displayed as cards

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Access environment variables
 const API_KEY = `live_gecybbtg4DMF0IwPFn5AC9xqPKEhz02jvj2EcTPI8lXWit3A16xGC4CDI9V6EYA2`;
-const API_URL = `https://api.thedogapi.com/v1/images/search?limit=10&api_key=${API_KEY}`;
+const API_URL = `https://api.thedogapi.com/v1/images/search?limit=12&api_key=${API_KEY}`;
 const GET_BUTTON = document.querySelector('#get_button');
 
 async function fetchPost() {
@@ -14,20 +14,30 @@ async function fetchPost() {
   }
 }
 
-
 // Function to display data in the #results div
 function displayResults(data) {
   const results_div = document.querySelector('#results');
   if (Array.isArray(data)) {
-    results_div.innerHTML = data.map(
-        (item) => `<img src="${item.url}" alt="Dog Image" />`).join('');
-  }
-  else {
-    results_div.innerHTML = `<p style="color: red;">Invalid data format.</p>`;
+    results_div.innerHTML = data
+    .map(
+        (item) => `
+        <div class="col-md-4 mb-4">
+          <div class="card">
+            <img src="${item.url}" class="card-img-top" alt="Dog Image" />
+            <div class="card-body">
+              <h5 class="card-title">A lovely dog!</h5>
+              <p class="card-text">This dog is looking for attention! Isn't it adorable?</p>
+            </div>
+          </div>
+        </div>`
+    )
+     .join('');
+  } else {
+    results_div.innerHTML = `<p class="invalid">Invalid data format.</p>`;
   }
 }
 
-// Function to demonstrate both GET and POST functionality
+// Function to demonstrate both GET functionality
 async function runProject() {
   try {
     console.log('Fetching all posts...');
